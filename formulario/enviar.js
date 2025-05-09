f1ccff391be6bc413531a86a8a1b955b35b144fb
fabiowakisaka <64746949+fabiowakisaka@users.noreply.github.com>
2025-05-09 15:20:21 -0300

    Update enviar.js

--- formulario/enviar.js
+++ formulario/enviar.js
@@ -9,7 +9,7 @@ document.getElementById('formulario').addEventListener('submit', async function 
   const repositorio = 'comofazeremprestimo';
 
   // Personal Access Token com escopo 'repo'
-  const token = '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';  // coloque temporariamente para testes locais
+  const token = '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';  // coloque temporariamente para testes locais
 
   const response = await fetch(`https://api.github.com/repos/${usuario}/${repositorio}/dispatches`, {
     method: 'POST',
